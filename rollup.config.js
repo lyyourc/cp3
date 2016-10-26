42edcb58e73c486f09ea7475251f54b0d4ff740b
Drake Leung <drakeleung42@gmail.com>
2016-10-26 20:53:45 +0800

    fix(rollup): build in different env
    rollup 根据环境变量 NODE_ENV 来打包

--- rollup.config.js
+++ rollup.config.js
@@ -1,13 +1,16 @@
 import serve from 'rollup-plugin-serve'
 import livereload from 'rollup-plugin-livereload'
 
+const env = process.env.NODE_ENV
+
+const plugins = env === 'prod'
+  ? []
+  : [livereload('dist'), serve()]
+
 export default {
   entry: 'src/main.js',
   format: 'umd',
   dest: 'dist/cp3.js',
   moduleName: 'cp3',
-  plugins: [
-    livereload('dist'),
-    serve()
-  ]
+  plugins,
 }
